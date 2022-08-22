2c21642bedf0f701e3da04df67941b6995601b08
sharmin1988 <rasnasharmin11@gmail.com>
2022-08-22 15:04:40 +0600

    total expence btn dinamic and utilities file make

--- js/per-player-budget.js
+++ js/per-player-budget.js
@@ -1,18 +1,15 @@
-function getInputFieldValueById(inputFieldId){
-   const inputField = document.getElementById(inputFieldId);
-   const inputFieldValueString = inputField.value ;
-   const inputFieldValue = parseFloat(inputFieldValueString);
-   return inputFieldValue;
 
-}
-
-function setTexElementValueById(elementId, value){
-    const textElement = document.getElementById(elementId);
-    textElement.innerText = value ;
-}
 
 document.getElementById('btn-pre-player').addEventListener('click', function(){
     const perPlayerBudget = getInputFieldValueById('per-player-input-field');
     const totalPlayerExpence = perPlayerBudget * 5;
-    setTexElementValueById('player-expence', totalPlayerExpence)
+    setTextElementValueById('player-expence', totalPlayerExpence)
+})
+
+document.getElementById('btn-total').addEventListener('click', function(){
+    const totalPlayerExpence = getTextElementValueById('player-expence');
+    const managerExpence = getInputFieldValueById('manager-expence');
+    const coachExpence = getInputFieldValueById('coach-expence');
+    const totalExpence = totalPlayerExpence + managerExpence + coachExpence;
+    setTextElementValueById('total-expence', totalExpence);
 })
---
+++ js/utilities.js
@@ -0,0 +1,19 @@
+function getInputFieldValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputField.value ;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    return inputFieldValue;
+ 
+ }
+ 
+ function setTextElementValueById(elementId, value){
+     const textElement = document.getElementById(elementId);
+     textElement.innerText = value ;
+ }
+ 
+ function getTextElementValueById(elementId){
+     const textElement = document.getElementById(elementId);
+     const textElementValueString = textElement.innerText;
+     const textElementValue = parseFloat(textElementValueString);
+     return textElementValue;
+ }
